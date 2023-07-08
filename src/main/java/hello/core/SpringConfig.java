112db08d9a9ecbf8038fdc52d1b2955348bb31a9
wodls1616 <wodls1616@gmail.com>
2023-07-08 16:47:32 +0900

    수업을 다시 JPA와 관련된 기본 Spring boot 공부

---
+++ src/main/java/hello/core/SpringConfig.java
@@ -0,0 +1,32 @@
+package hello.core;
+
+import hello.core.repository.JdbcMemberRepository;
+import hello.core.repository.MemberRepository;
+import hello.core.repository.MemoryMemberRepository;
+import hello.core.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    /*@Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }*/
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+    }
+}
---
+++ src/test/java/hello/core/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package hello.core.repository;
+
+import hello.core.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @DisplayName("회원 저장")
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        //assertThat(member).isEqualTo(result);
+        assertEquals(member, result);
+    }
+
+
+    @DisplayName("이름으로 회원찾기")
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+
+    }
+
+
+    @DisplayName("전체 회원 조회")
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result =  repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
